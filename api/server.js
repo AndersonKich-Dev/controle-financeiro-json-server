cb5c12d1aac113fab3d2ef3918e829a80f779c59
Anderson kich <andersonkichdev@gmail.com>
2023-06-02 08:15:52 -0300

    update routes

--- api/server.js
+++ api/server.js
@@ -1,11 +1,10 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("routes.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
