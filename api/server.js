c8cd335e1f8342ff52f562a7a7846e7cc2a5fb9b
Anderson kich <andersonkichdev@gmail.com>
2023-06-02 08:33:29 -0300

    config vercel

--- api/server.js
+++ api/server.js
@@ -1,14 +1,22 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("routes.json");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+// Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
     "/blog/:resource/:id/show": "/:resource/:id",
+    "/employees/list": "/employees",
+    "/employees/get/:id": "/employees/:id",
+    "/employees/create": "/employees",
+    "/employees/update/:id": "/employees/:id",
+    "/employees/delete/:id": "/employees/:id",
+
+    "/auth/login": "/login",
   })
 );
 server.use(router);
